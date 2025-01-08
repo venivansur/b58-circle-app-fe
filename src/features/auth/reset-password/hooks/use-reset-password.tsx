d25e4381602beaa265f4382738bacc369f4a29bd
unknown <vansurya.01@gmail.com>
2025-01-08 09:52:41 +0700

    fix vercell

--- src/features/auth/reset-password/hooks/use-reset-password.tsx
+++ src/features/auth/reset-password/hooks/use-reset-password.tsx
@@ -37,7 +37,7 @@ export const useResetForm = () => {
       }
 
       try {
-        const response = await axios.post('http://localhost:4000/api/v1/auth/reset-password', {
+        const response = await axios.post('b58-circle-app-be-venivansuryas-projects.vercel.app/api/v1/auth/reset-password', {
           token,
           password: data.password,
         });
--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,17 +2,20 @@ import axios from "axios";
 
 
 const api = axios.create({
-  baseURL: "http://localhost:4000/api/v1",
+  baseURL: import.meta.env.VITE_API_URL || "b58-circle-app-be-venivansuryas-projects.vercel.app/api/v1",
 });
 
 
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");
- 
+
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
+
   return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export { api };
